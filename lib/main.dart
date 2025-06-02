547accadd4bdf3b0996dd20a6e94fc291b11f94d
technicalmeaw <technicalmeaw@gmail.com>
2025-06-03 01:16:07 +0530

    improved SEO and added initial loader screen boot animation

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget  {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Santanu Mukherjee',
+      title: 'Santanu Mukherjee | Software Developer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
